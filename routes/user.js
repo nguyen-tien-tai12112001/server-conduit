d7223419aa5361cda3278f0e6582f426c9be4d18
nguyen tien tai <anhtaibnvn12112001@gmail.com>
2021-12-08 14:32:30 +0700

    update

--- routes/user.js
+++ routes/user.js
@@ -5,7 +5,7 @@ import { signin, signup, getUser, updateProfile } from '../controllers/user.js';
 
 router.post('/signin', signin);
 router.post('/signup', signup);
-router.post('/profile/:id', updateProfile);
+router.put('/profile/:id', updateProfile);
 router.get('/', getUser);
 
 export default router;
